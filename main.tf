143b0c9bee622ee504394463394434e96c3df5c1
Dominik Deren <dominik.deren@live.com>
2021-11-07 16:59:16 +0100

    Re-enabling tests & adding CW Agent for collecting mem & disk metrics.

--- main.tf
+++ main.tf
@@ -18,7 +18,85 @@ locals {
 
   user_data = <<-EOT
   #!/bin/bash
-  
+
+  echo "Installing CW Agent"
+  sudo yum install amazon-cloudwatch-agent -y
+
+  echo "Creating CW Agent config file"
+  cat > /opt/aws/amazon-cloudwatch-agent/bin/config.json <<EOL
+  {
+    "agent": {
+      "metrics_collection_interval": 60,
+      "run_as_user": "root"
+    },
+    "metrics": {
+      "append_dimensions": {
+        "AutoScalingGroupName": "$${aws:AutoScalingGroupName}",
+        "ImageId": "$${aws:ImageId}",
+        "InstanceId": "$${aws:InstanceId}",
+        "InstanceType": "$${aws:InstanceType}"
+      },
+      "metrics_collected": {
+        "cpu": {
+          "measurement": [
+            "cpu_usage_idle",
+            "cpu_usage_iowait",
+            "cpu_usage_user",
+            "cpu_usage_system"
+          ],
+          "metrics_collection_interval": 60,
+          "totalcpu": false
+        },
+        "disk": {
+          "measurement": [
+            "used_percent",
+            "inodes_free"
+          ],
+          "metrics_collection_interval": 60,
+          "resources": [
+            "*"
+          ]
+        },
+        "diskio": {
+          "measurement": [
+            "io_time",
+            "write_bytes",
+            "read_bytes",
+            "writes",
+            "reads"
+          ],
+          "metrics_collection_interval": 60,
+          "resources": [
+            "*"
+          ]
+        },
+        "mem": {
+          "measurement": [
+            "mem_used_percent"
+          ],
+          "metrics_collection_interval": 60
+        },
+        "netstat": {
+          "measurement": [
+            "tcp_established",
+            "tcp_time_wait"
+          ],
+          "metrics_collection_interval": 60
+        },
+        "swap": {
+          "measurement": [
+            "swap_used_percent"
+          ],
+          "metrics_collection_interval": 60
+        }
+      }
+    }
+  }
+  EOL
+
+  echo "Starting CW Agent"
+  sudo /opt/aws/amazon-cloudwatch-agent/bin/amazon-cloudwatch-agent-ctl -a fetch-config -m ec2 -s -c file:/opt/aws/amazon-cloudwatch-agent/bin/config.json
+
   echo "Creating directory for deso."
   mkdir /deso-node
 
